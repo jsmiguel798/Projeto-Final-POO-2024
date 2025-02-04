d1b3797871057a5ca9491fb63c079f7db0a38c07
jsmiguel798 <joselima2023.11@gmail.com>
2025-02-04 11:47:26 -0300

    Criando atributos da classe medico

--- src/classes/Medico.java
+++ src/classes/Medico.java
@@ -1,5 +1,41 @@
 package classes;
 
 public class Medico {
+	private String nome;
+	private String crm;
+	private String especialidade;
+	private String telefone;
+	private String email;
 
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCrm() {
+		return crm;
+	}
+	public void setCrm(String crm) {
+		this.crm = crm;
+	}
+	public String getEspecialidade() {
+		return especialidade;
+	}
+	public void setEspecialidade(String especialidade) {
+		this.especialidade = especialidade;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}	
 }
