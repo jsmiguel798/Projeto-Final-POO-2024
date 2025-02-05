7e6d5917250091e391a603d4de71e6cdb6ac01a3
jsmiguel798 <joselima2023.11@gmail.com>
2025-02-05 10:16:33 -0300

    Criação da classe clinica

---
+++ src/classes/Consulta.java
@@ -0,0 +1,58 @@
+package classes;
+
+public class Consulta {
+
+	private Paciente paciente;
+	private Medico medico;
+	private String data;
+	private String horario;
+	private String diagnostico;
+
+	public Consulta(Paciente paciente, Medico medico, String data, String horario, String diagnostico) {
+		this.paciente = paciente;
+		this.medico = medico;
+		this.data = data;
+		this.horario = horario;
+		this.diagnostico = diagnostico;
+	}
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+
+	public Medico getMedico() {
+		return medico;
+	}
+
+	public void setMedico(Medico medico) {
+		this.medico = medico;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getHorario() {
+		return horario;
+	}
+
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+
+	public String getDiagnostico() {
+		return diagnostico;
+	}
+
+	public void setDiagnostico(String diagnostico) {
+		this.diagnostico = diagnostico;
+	}
+}
