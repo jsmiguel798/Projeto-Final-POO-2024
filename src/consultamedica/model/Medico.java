e4a4f1277b3879b72a3d4e4f6f72214c5be40dab
jsmiguel798 <joselima2023.11@gmail.com>
2025-02-18 21:14:38 -0300

    classes movida para o modelo

---
+++ src/consultamedica/model/Medico.java
@@ -0,0 +1,58 @@
+package consultamedica.model;
+
+public class Medico {
+	private String nome;
+	private String crm;
+	private String especialidade;
+	private String telefone;
+	private String email;
+
+	public Medico(String nome, String crm, String especialidade, String telefone, String email) {
+		this.nome = nome;
+		this.crm = crm;
+		this.especialidade = especialidade;
+		this.telefone = telefone;
+		this.email = email;
+
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCrm() {
+		return crm;
+	}
+
+	public void setCrm(String crm) {
+		this.crm = crm;
+	}
+
+	public String getEspecialidade() {
+		return especialidade;
+	}
+
+	public void setEspecialidade(String especialidade) {
+		this.especialidade = especialidade;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
---
+++ src/consultamedica/model/Paciente.java
@@ -0,0 +1,50 @@
+package consultamedica.model;
+
+public class Paciente {
+
+	private String nome;
+	private String telefone;
+	private String dataNascimento;
+	private String historicoMedico;
+
+	public Paciente(String nome, String telefone, String dataNascimento, String historicoMedico) {
+		this.nome = nome;
+		this.telefone = telefone;
+		this.dataNascimento = dataNascimento;
+		this.historicoMedico = historicoMedico;
+
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getHistoricoMedico() {
+		return historicoMedico;
+	}
+
+	public void setHistoricoMedico(String historicoMedico) {
+		this.historicoMedico = historicoMedico;
+	}
+
+}
