6633d8215dcc1f809a45c8dedff3d0837a3ade77
jsmiguel798 <joselima2023.11@gmail.com>
2025-02-20 10:56:16 -0300

    listar e criar paciente no PacienteDAO

--- src/consultamedica/dao/PacienteDAO.java
+++ src/consultamedica/dao/PacienteDAO.java
@@ -1,5 +1,16 @@
 package consultamedica.dao;
 
-public class PacienteDAO {
+import java.util.ArrayList;
+import consultamedica.model.*;
 
+public class PacienteDAO {
+	private static ArrayList<Paciente> pacientes = new ArrayList<Paciente>();
+	
+	public static void create(Paciente paciente) {
+		pacientes.add(paciente);
+	}
+	
+	public static ArrayList<Paciente> list() {		
+		return pacientes;
+	}
 }
