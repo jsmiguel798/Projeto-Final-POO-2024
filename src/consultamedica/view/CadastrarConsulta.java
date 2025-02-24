d12218b1e875cc978decc30241a759ce7e0e3b26
jsmiguel798 <joselima2023.11@gmail.com>
2025-02-24 07:09:23 -0300

    Jframe De Cadastrar Consulta

---
+++ src/consultamedica/view/CadastrarConsulta.java
@@ -0,0 +1,35 @@
+package consultamedica.view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class CadastrarConsulta extends JFrame {
+
+	private JPanel contentPane;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CadastrarConsulta frame = new CadastrarConsulta();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public CadastrarConsulta() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
