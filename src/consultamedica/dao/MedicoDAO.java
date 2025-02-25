9c018a78a602f43066789b2e4c549831b4590a03
jsmiguel798 <joselima2023.11@gmail.com>
2025-02-25 08:36:54 -0300

    MedicoDAO

--- src/consultamedica/dao/MedicoDAO.java
+++ src/consultamedica/dao/MedicoDAO.java
@@ -5,15 +5,24 @@ import consultamedica.model.*;
 
 public class MedicoDAO {
 	private static ArrayList<Medico> medicos = new ArrayList<Medico>();
-	
+
 	public static void create(Medico medico) {
 		medicos.add(medico);
 	}
-	
+
 	public static ArrayList<Medico> list() {
 		return medicos;
 	}
+
 	public static void delete(int linha) {
 		medicos.remove(linha);
 	}
-}
+
+	public static Medico getMedico(int linha) {
+		return medicos.get(linha);
+
+	}
+	public static void update(Medico medicos, int linha) {
+	
+		}
+	}
